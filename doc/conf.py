048a758060da5e9a88d900338b32cc3da454cef8
Henry Whitehead <whiteheadh@asmaclap55.local>
2024-05-23 16:02:36 +0100

    fixed file path to images

--- doc/conf.py
+++ doc/conf.py
@@ -6,9 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Example'
-copyright = 'workshop participant'
-author = 'workshop participant'
+project = 'EXOTIC'
+copyright = 'none'
+author = 'Luke Parker, Henry Whitehead'
 release = '0.1'
 
 
